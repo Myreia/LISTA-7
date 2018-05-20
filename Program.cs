a616b533f8965f2e113c8cbb52c62ef68d6f18c1
Myreia <39448981+Myreia@users.noreply.github.com>
2018-05-19 23:52:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              Console.WriteLine("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬");
+            Console.WriteLine("¬¬¬¬¬¬¬¬¬¬¬ Multiplos de 7 de 0 á 1000 ¬¬¬¬¬¬¬¬¬¬¬");
+            Console.WriteLine("¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬");
+
+            for (int i = 0; i <= 1000; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine("\n\n" + i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
